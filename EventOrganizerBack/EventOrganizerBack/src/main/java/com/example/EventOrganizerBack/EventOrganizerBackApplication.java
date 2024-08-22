cfbd15a703656aec125cad749fcb087aff26fda6
Quentin Jarry <quent1_jarry@hotmail.com>
2024-08-22 16:39:54 +0200

    init backend fail launch

---
+++ EventOrganizerBack/EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/EventOrganizerBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.EventOrganizerBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrganizerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventOrganizerBackApplication.class, args);
+	}
+
+}
