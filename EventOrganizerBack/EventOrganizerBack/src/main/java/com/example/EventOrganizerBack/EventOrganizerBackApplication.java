c4eb652e7b68dd0595aa6760e2042a0de7bd31bf
Quentin Jarry <quent1_jarry@hotmail.com>
2024-08-22 16:44:45 +0200

    disable DataSourceAuto for bare launch

--- EventOrganizerBack/EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/EventOrganizerBackApplication.java
+++ EventOrganizerBack/EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/EventOrganizerBackApplication.java
@@ -2,8 +2,9 @@ package com.example.EventOrganizerBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EventOrganizerBackApplication {
 
 	public static void main(String[] args) {
