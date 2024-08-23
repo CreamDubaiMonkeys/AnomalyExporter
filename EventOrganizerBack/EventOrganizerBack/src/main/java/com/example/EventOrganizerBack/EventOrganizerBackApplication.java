a3a2d1b0ab1c21d0c1ba8113f44ff62ef8a9b5db
Quentin Jarry <quent1_jarry@hotmail.com>
2024-08-23 12:15:40 +0200

    setup ground database connection -not working

--- EventOrganizerBack/EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/EventOrganizerBackApplication.java
+++ EventOrganizerBack/EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/EventOrganizerBackApplication.java
@@ -2,9 +2,8 @@ package com.example.EventOrganizerBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class EventOrganizerBackApplication {
 
 	public static void main(String[] args) {
