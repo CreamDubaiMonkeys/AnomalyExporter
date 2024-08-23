c6aa68c72ebbf4b492047ec72ff5b9d9d4e55bb8
Guy <gmabyalaht@gmail.com~>
2024-08-23 15:55:17 +0200

    Ajout du NgForm pour la gestion des données

--- EventOrganizerFront/src/app/inscription/inscription.component.ts
+++ EventOrganizerFront/src/app/inscription/inscription.component.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.css'
 })
 export class InscriptionComponent {
-
+    onSubmit(form: NgForm) {
+      console.log(form.value); 
+    }
 }
