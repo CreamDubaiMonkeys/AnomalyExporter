d4f0b82112147ba5fdd6db86726794c17fe9b885
AdamIfnou <200~40129364+adamifnou@users.noreply.github.com>
2024-08-26 12:05:47 +0200

    setup material ui

--- EventOrganizerFront/src/main.ts
+++ EventOrganizerFront/src/main.ts
@@ -3,7 +3,8 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {provideRouter} from '@angular/router';
 import AppRoutingModule from './app/routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideProtractorTestingSupport(), provideRouter(AppRoutingModule)],
+  providers: [provideProtractorTestingSupport(), provideRouter(AppRoutingModule), provideAnimationsAsync()],
 }).catch((err) => console.error(err));
