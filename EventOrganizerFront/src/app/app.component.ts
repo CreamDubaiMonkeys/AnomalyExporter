333fd48551f996fc4e4e3bc5da98b258f4cb6c66
Guy <gmabyalaht@gmail.com~>
2024-08-26 17:18:12 +0200

    resolveConflit

--- EventOrganizerFront/src/app/app.component.ts
+++ EventOrganizerFront/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { RouterModule } from '@angular/router';
 import { CalendarModule } from './calendar/calendar.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InscriptionComponent, RouterModule, LoginComponent, CalendarModule],
+  imports: [RouterOutlet, InscriptionComponent, RouterModule, LoginComponent, NavbarComponent, CalendarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
