760335b4133bfc7c7a62bd5e4fafb3769e8bc19d
Quentin Jarry <quent1_jarry@hotmail.com>
2024-08-27 00:32:55 +0200

    add calendar v0

--- EventOrganizerFront/src/app/app.component.ts
+++ EventOrganizerFront/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { RouterModule } from '@angular/router';
+import { CalendarModule } from './calendar/calendar.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InscriptionComponent, RouterModule, LoginComponent],
+  imports: [RouterOutlet, InscriptionComponent, RouterModule, LoginComponent, CalendarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ EventOrganizerFront/src/app/calendar/calendar.module.ts
@@ -0,0 +1,24 @@
+import {DataService} from "./calendar.service";
+import {FormsModule} from "@angular/forms";
+import {NgModule} from "@angular/core";
+import {CalendarComponent} from "./calendar.component";
+import {DayPilotModule} from "@daypilot/daypilot-lite-angular";
+import {provideHttpClient} from "@angular/common/http";
+import {CommonModule} from "@angular/common";
+
+@NgModule({
+  imports:      [
+    CommonModule,
+    FormsModule,
+    DayPilotModule
+  ],
+  declarations: [
+    CalendarComponent
+  ],
+  exports:      [ CalendarComponent ],
+  providers:    [
+    DataService,
+    provideHttpClient()
+  ]
+})
+export class CalendarModule { }
