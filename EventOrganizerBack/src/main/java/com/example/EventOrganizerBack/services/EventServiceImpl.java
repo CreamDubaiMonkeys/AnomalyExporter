bc52c1963bb0684b863d3a4f7b2ad6ddb6f787ec
AdamIfnou <200~40129364+adamifnou@users.noreply.github.com>
2024-08-27 11:38:00 +0200

    added get put and delete endpoints to event controller

--- EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/services/EventServiceImpl.java
+++ EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/services/EventServiceImpl.java
@@ -1,15 +1,16 @@
 package com.example.EventOrganizerBack.services;
 
-import com.example.EventOrganizerBack.model.User;
 import com.example.EventOrganizerBack.repository.EventRepository;
 import com.example.EventOrganizerBack.dto.EventDto;
 import com.example.EventOrganizerBack.model.Event;
 import com.example.EventOrganizerBack.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EventServiceImpl implements EventService{
@@ -21,7 +22,7 @@ public class EventServiceImpl implements EventService{
         this.userRepository = userRepository;
     }
     @Override
-    public   Event createEvent(EventDto toAddEvent){
+    public Event createEvent(@RequestBody EventDto toAddEvent){
         Event event = new Event();
         if(toAddEvent != null){
             event.setCreator(userRepository.findById(toAddEvent.getCreatorId()).orElse(null));
@@ -38,6 +39,58 @@ public class EventServiceImpl implements EventService{
             eventRepository.save(event);
         }
 
+        return event;
+    }
+
+    @Override
+    public Event updateEvent(@RequestBody EventDto toUpdateEvent, @PathVariable Integer id){
+        Event event = eventRepository.findById(id).orElse(null);
+        if(event != null){
+            event.setCreator(userRepository.findById(toUpdateEvent.getCreatorId()).orElse(null));
+            event.setDate(toUpdateEvent.getDate());
+            event.setTime(toUpdateEvent.getTime());
+            event.setCapacity(toUpdateEvent.getCapacity());
+            event.setCreated_at(event.getCreated_at());
+            Timestamp timestamp = Timestamp.valueOf(LocalDateTime.now());
+            event.setUpdated_at(timestamp);
+            event.setIs_private(toUpdateEvent.getIs_private());
+            event.setDescription(toUpdateEvent.getDescription());
+            event.setTitle(toUpdateEvent.getTitle());
+            event.setLocation(toUpdateEvent.getLocation());
+            eventRepository.save(event);
+        }
+        return event;
+    }
+
+    @Override
+    public Event getEventById(Integer id){
+        return eventRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Event> getAllCreatorEvents(Integer creatorId){
+        List<Event> events = eventRepository.findAll();
+        //filter events with creator id
+        if (events != null){
+            events.removeIf(event -> !event.getCreator().getId().equals(creatorId));
+        }else{
+            events = null;
+        }
+
+        return events;
+    }
+
+    @Override
+    public List<Event> getAllEvents(){
+        return eventRepository.findAll();
+    }
+
+    @Override
+    public Event deleteEvent(Integer id){
+        Event event = eventRepository.findById(id).orElse(null);
+        if(event != null){
+            eventRepository.delete(event);
+        }
         return event;
     }
 }
