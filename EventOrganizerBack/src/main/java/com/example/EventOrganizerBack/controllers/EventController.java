e249cb7edc942de30226db7b14161c8cdf66bd1f
AdamIfnou <200~40129364+adamifnou@users.noreply.github.com>
2024-08-27 09:39:06 +0200

    added mapping

--- EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/controllers/EventController.java
+++ EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/controllers/EventController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/events")
 public class EventController {
     private final EventService eventService;
 
@@ -35,7 +37,7 @@ public class EventController {
 
 * */
 
-    @PostMapping("/event")
+    @PostMapping("/create")
     public ResponseEntity<Object> createEvent(@RequestBody EventDto eventDto) {
         try{
             Event createdEvent = eventService.createEvent(eventDto);
