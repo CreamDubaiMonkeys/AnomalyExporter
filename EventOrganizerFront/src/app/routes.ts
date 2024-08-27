7089e9461c30d67acc887749d2488cd7f1ac605e
Guy <gmabyalaht@gmail.com~>
2024-08-27 15:57:36 +0200

    login/logout sans back

--- EventOrganizerFront/src/app/routes.ts
+++ EventOrganizerFront/src/app/routes.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { HomeComponent } from './home/home.component';
+import { LoginGuard } from './login.guard';
 
 const AppRoutingModule: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: LoginComponent, canActivate: [LoginGuard] },
   { path: 'inscription', component: InscriptionComponent },
   { path: 'calendar', component: HomeComponent}
 ];
