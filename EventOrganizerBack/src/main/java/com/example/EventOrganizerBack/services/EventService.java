b5781f21ac0ef016cac647e10681652959ca512f
AdamIfnou <200~40129364+adamifnou@users.noreply.github.com>
2024-08-27 11:38:00 +0200

    added get put and delete endpoints to event controller

--- EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/services/EventService.java
+++ EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/services/EventService.java
@@ -3,6 +3,13 @@ package com.example.EventOrganizerBack.services;
 import com.example.EventOrganizerBack.dto.EventDto;
 import com.example.EventOrganizerBack.model.Event;
 
+import java.util.List;
+
 public interface EventService {
     Event createEvent(EventDto addedEvent);
+    Event updateEvent(EventDto updatedEvent, Integer id);
+    Event getEventById(Integer id);
+    List<Event> getAllCreatorEvents(Integer userId);
+    List<Event> getAllEvents();
+    Event deleteEvent(Integer id);
 }
