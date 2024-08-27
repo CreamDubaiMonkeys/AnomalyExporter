e0eaeef1ecf1c9c34c2990b8bf4d2311e4a7d8da
AdamIfnou <200~40129364+adamifnou@users.noreply.github.com>
2024-08-27 21:03:05 +0200

    created generic event table

--- EventOrganizerFront/src/app/second-home/second-home.component.ts
+++ EventOrganizerFront/src/app/second-home/second-home.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 import { MyEventsTableComponent } from './my-events-table/my-events-table.component';
 import { HttpProviderService } from '../service/http-provider.service';
+import { PublicEventsTableComponent } from './public-events-table/public-events-table.component';
 
 @Component({
   selector: 'app-second-home',
   standalone: true,
-  imports: [MyEventsTableComponent],
+  imports: [MyEventsTableComponent, PublicEventsTableComponent],
   templateUrl: './second-home.component.html',
   styleUrl: './second-home.component.css',
 })
@@ -17,6 +18,49 @@ export class SecondHomeComponent implements OnInit {
   ngOnInit():void {
     this.LoadEvents();
   }
+  LoadEvents() {
+    this.httpProviderService.getEventById(this.id).subscribe(
+      (res) => {
+        this.events = res.body;
+        console.log('Compartiments:', this.events);
+      },
+      (error) => {
+        console.error('Error fetching compartiments:', error);
+      }
+    );
+  }
+}
+*/
+import { Component, OnInit } from '@angular/core';
+import { HttpProviderService } from '../service/http-provider.service';
+import { EventsTableComponent } from './events-table/events-table.component';
+
+@Component({
+  selector: 'app-second-home',
+  standalone: true,
+  imports: [ EventsTableComponent],
+  templateUrl: './second-home.component.html',
+  styleUrl: './second-home.component.css',
+})
+export class SecondHomeComponent implements OnInit {
+  events: any[] = [];
+  displayedColumns: string[] = [
+    'title',
+    'date',
+    'time',
+    'capacity',
+    'created_at',
+    'description',
+    'location',
+  ];
+  id = 1;
+
+  constructor(private httpProviderService: HttpProviderService) {}
+
+  ngOnInit(): void {
+    this.LoadEvents();
+  }
+
   LoadEvents() {
     this.httpProviderService.getEventById(this.id).subscribe(
       (res) => {
