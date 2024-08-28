96b2484d4b6b10e573ab43f9ff4a3498ddd9a141
AdamIfnou <200~40129364+adamifnou@users.noreply.github.com>
2024-08-28 10:48:39 +0200

    Configure CORS globally

--- EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/controllers/EventController.java
+++ EventOrganizerBack/src/main/java/com/example/EventOrganizerBack/controllers/EventController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/events")
-@CrossOrigin(origins = "http://localhost:4200")
 public class EventController {
     private final EventService eventService;
 
